0a21f6fc1d448b13f3413c2c1eba41920020a07b
Michał <michal.talarczyk@yahoo.com>
2021-06-11 10:47:15 +0200

    3rd commit

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
-const add = (a, b) => a + b;
+const { promisify } = require("util");
+const exec = promisify(require("child_process").exec);
+
+// if (process.argv.length > 2) {
+//     const ipArr = process.argv.splice(2);
+
+//     Promise.all(
+//         ipArr.map(async (ip) => {
+//             // check if ip is valid
+//             const ipRegEx = /((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}/;
+//             if (ipRegEx.test(ip)) {
+//                 console.log("Valid ip:", ip);
+//                 const { stderr, stdout } = await exec(`ping ${ip}`);
+//                 if (!stderr) {
+//                     console.log(stdout);
+//                 } else {
+//                     console.log(stderr);
+//                 }
+//             } else {
+//                 console.log("Not valid ip:", ip);
+//             }
+//         })
+//     );
+// }
+
+const openApp = () => {
+    if (process.argv.length > 2) {
+        const app = process.argv[2];
+        switch (app) {
+          case "Kalkulator":
+            exec("calc.exe").then((stdout, stderr) => {
+              console.log(stdout);
+            });
+            break;
+          case "Paint":
+            exec("mspaint.exe").then((stdout, stderr) => {
+              console.log(stdout);
+            });
+            break;
+          default:
+            console.log("Invalid app name.");
+        }
+    }
+}
 
 module.exports = {
-    add,
+    openApp,
 };
